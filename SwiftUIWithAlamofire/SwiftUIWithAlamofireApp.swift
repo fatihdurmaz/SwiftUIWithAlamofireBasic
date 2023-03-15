7c9eb6810a7b1d0a4a3e99ed8f658a8ba54a68ec
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-03-16 00:04:29 +0300

    Initial Commit

---
+++ SwiftUIWithAlamofire/SwiftUIWithAlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithAlamofireApp.swift
+//  SwiftUIWithAlamofire
+//
+//  Created by Fatih Durmaz on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithAlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
