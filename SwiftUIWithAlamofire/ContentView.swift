7747026a9269cbcc17918486ca06760e83011836
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-03-16 01:22:06 +0300

    first commit

--- SwiftUIWithAlamofire/ContentView.swift
+++ SwiftUIWithAlamofire/ContentView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct ContentView: View {
+    
+    @State var users = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            List(users){ user in
+                HStack{
+                    Text("\(user.id)")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.gray)
+                        .clipShape(Circle())
+                    VStack(alignment: .leading){
+                        
+                        Text(user.name)
+                            .font(.headline)
+                        Text(user.email)
+                            .font(.subheadline)
+                    }
+                }
+            }
+            .onAppear(perform: loadData)
+            .navigationTitle("SwiftUI & Alamofire")
         }
-        .padding()
+    }
+    
+    func loadData(){
+        AF.request("https://jsonplaceholder.typicode.com/users")
+            .responseDecodable(of:[User].self){
+                response in
+                guard let users = response.value else {return}
+                self.users = users
+            }
     }
 }
 
